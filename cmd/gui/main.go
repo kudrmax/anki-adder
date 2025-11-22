c878f3b87f150908b5718988a43ae23421a6bd80
Maksim Kudryashov <mdmkudryashov@avito.ru>
2025-11-22 21:43:16 +0300

    Add GUI with GPT

---
+++ cmd/gui/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/jroimartin/gocui"
+)
+
+func main() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.Cursor = true
+	g.Mouse = true
+
+	g.SetManagerFunc(layout)
+
+	// Global quit keybinding: Ctrl+C
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	// Submit with Enter when focus is on the input line
+	if err := g.SetKeybinding("input", gocui.KeyEnter, gocui.ModNone, submitInput); err != nil {
+		log.Panicln(err)
+	}
+
+	// Submit with mouse click on the button
+	if err := g.SetKeybinding("button", gocui.MouseLeft, gocui.ModNone, submitInput); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+
+	// ---- Input view (centered) ----
+	inputWidth := maxX / 2
+	inputHeight := 2
+
+	x0 := (maxX - inputWidth) / 2
+	x1 := x0 + inputWidth
+	y0 := (maxY - inputHeight) / 2
+	y1 := y0 + inputHeight
+
+	if v, err := g.SetView("input", x0, y0, x1, y1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = " Write a sentence "
+		v.Editable = true
+		v.Editor = gocui.DefaultEditor
+		v.Frame = true
+
+		if _, err := g.SetCurrentView("input"); err != nil {
+			return err
+		}
+	}
+
+	// ---- Button view (under the input) ----
+	btnWidth := 13
+	if btnWidth > maxX-2 {
+		btnWidth = maxX - 2
+	}
+
+	bx0 := (maxX - btnWidth) / 2
+	bx1 := bx0 + btnWidth
+	by0 := y1 + 1
+	by1 := by0 + 2
+
+	if v, err := g.SetView("button", bx0, by0, bx1, by1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Frame = true
+		v.Editable = false
+
+		// Text inside the button view
+		fmt.Fprintln(v, "   Submit   ")
+	}
+
+	return nil
+}
+
+// submitInput is called when the user presses Enter in the input
+// or clicks on the button with the mouse.
+func submitInput(g *gocui.Gui, v *gocui.View) error {
+	inputView, err := g.View("input")
+	if err != nil {
+		return err
+	}
+
+	// Read and trim the input text
+	text := strings.TrimSpace(inputView.Buffer())
+
+	// Call your stub function
+	doSomething(text)
+
+	// Clear input and reset cursor
+	inputView.Clear()
+	if err := inputView.SetCursor(0, 0); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// doSomething is a stub where you can put your own logic.
+func doSomething(s string) {
+	//log.Printf("doSomething called with: %q", s)
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
--- go.mod
+++ go.mod
@@ -19,11 +19,14 @@ require (
 	github.com/go-resty/resty/v2 v2.7.0 // indirect
 	github.com/golang/protobuf v1.3.3 // indirect
 	github.com/jarcoal/httpmock v1.0.8 // indirect
+	github.com/jroimartin/gocui v0.5.0 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
